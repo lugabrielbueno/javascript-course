afd6bb8ba66f381ffbcccc5680afeedc4949ce1e
Gabriel Bueno <gabriel-euro@hotmail.com.br>
2020-08-25 20:18:47 -0300

    bootcamp web dev

---
+++ javascript/exercise1/exercise1.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What's your first name? ");
+var lastName = prompt("What's your last name? ");
+var age = prompt("How old are you? ");
+
+console.log('Your name is '+firstName+' ' +lastName+' and your age is '+age+' years old');
---
+++ javascript/exercise3/exercise3.js
@@ -0,0 +1,5 @@
+function test(x,y) {
+    return y-x;
+}
+var value = test(10,40);
+console.log(value);
